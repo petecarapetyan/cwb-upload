91529694128666544f2977140d9dff59e1f83fd2
petecarapetyan <pete@datafundamentals.com>
2021-08-02 17:32:06 -0500

    WIP

--- src/state/firebase/config.ts
+++ src/state/firebase/config.ts
@@ -1,8 +1,8 @@
 export const config = {
-  apiKey: "AIzaSyBrsKvHRuu-7ATc5A02Ywcdqy72tYZJAeo",
-  authDomain: "bella-strati.firebaseapp.com",
-  projectId: "bella-strati",
-  storageBucket: "bella-strati.appspot.com",
-  messagingSenderId: "1049914168860",
-  appId: "1:1049914168860:web:662863fabe658f4e6c31d9"
-}
+  apiKey: "AIzaSyDw9mG-WxkEFZGwcKZXpUz1pthDsII2KTE",
+  authDomain: "fauxmazon.firebaseapp.com",
+  projectId: "fauxmazon",
+  storageBucket: "fauxmazon.appspot.com",
+  messagingSenderId: "141282400252",
+  appId: "1:141282400252:web:81167359ce23b970f40479"
+};
